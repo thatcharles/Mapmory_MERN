90ffcd6175df699491164931494e14c430e18a91
Charles Chung <charles5j21z@gmail.com>
2020-03-29 19:34:00 -0400

    new build

---
+++ client/build/precache-manifest.405a1107c500ae8f6740d571416eb646.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f44ddb62f735c3f4cf8a3727a0c72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41b5bc0b80374415293",
+    "url": "/static/css/2.58775449.chunk.css"
+  },
+  {
+    "revision": "0e987cbf351cbcef66b1",
+    "url": "/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "a41b5bc0b80374415293",
+    "url": "/static/js/2.077d94e0.chunk.js"
+  },
+  {
+    "revision": "0a31e8d8bf762c264a85136ccec70d7f",
+    "url": "/static/js/2.077d94e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e987cbf351cbcef66b1",
+    "url": "/static/js/main.c1e6f0e0.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "e95b2e68e3e42ff1fabaf8ca633258de",
+    "url": "/static/media/blue_circle.e95b2e68.svg"
+  },
+  {
+    "revision": "1409de5e4977b8a60886d9dfbd160784",
+    "url": "/static/media/la_6.1409de5e.jpg"
+  },
+  {
+    "revision": "36a6661f71bdb6a562cf6e7f61bc2470",
+    "url": "/static/media/parking.36a6661f.svg"
+  },
+  {
+    "revision": "9399cadf42a1cb70fd051b00ff1a13c7",
+    "url": "/static/media/place.9399cadf.svg"
+  }
+]);
