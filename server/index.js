b9e563a788b0451ad42af7a11081eed1f2b1612f
Charles Chung <charles5j21z@gmail.com>
2020-03-29 19:23:53 -0400

    modified

--- server/index.js
+++ server/index.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // index.html for all page routes
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
